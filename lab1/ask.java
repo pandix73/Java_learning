e724fa7a73f1277335037d52e57f75d85f2eaaf0
pandix73 <conan11conan112004@gmail.com>
2016-02-26 02:48:05 +0800

    software_studio_lab1

---
+++ lab1/ask.java
@@ -0,0 +1,21 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class ask {
+	
+	public static void main( String[] args )
+	{
+	// TODO: Create an instance of Student class.
+		student s = new student();
+	// Give this student a name.
+	// TODO: Change the name.
+		Scanner scanner = new Scanner(System.in);
+		
+		s.setName(scanner.next());
+
+	// Call the introduce method.
+		s.introduce();
+	}
+}
+
